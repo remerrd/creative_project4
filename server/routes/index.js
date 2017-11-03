1d532c3f5ca6b4b92583f7cb7fdfa045b31a19c1
Ryan Remer <remerrd@byu.edu>
2017-11-02 18:24:55 -0600

    routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+router.get('/directory',function(req, res) {
+  console.log("In directory");
 });
 
 module.exports = router;
